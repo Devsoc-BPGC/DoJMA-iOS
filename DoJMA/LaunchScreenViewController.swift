3d0ae9f8a8e09f6192a0bc6389e220f359c892e5
raghavprasad13 <31107116+raghavprasad13@users.noreply.github.com>
2018-07-22 21:48:22 +0530

    Setup main basic ViewControllers for Herald, Gazette, Events and Utilities, embedded in a TabBarControllerr

---
+++ DoJMA/LaunchScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LaunchScreenViewController.swift
+//  DoJMA
+//
+//  Created by Raghav Prasad on 22/07/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+            self.performSegue(withIdentifier: "showMainScreen", sender: self)
+        })
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+            self.performSegue(withIdentifier: "showMainScreen", sender: self)
+        })
+    }
+}
+
